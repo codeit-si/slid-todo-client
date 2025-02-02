8afb7d4249e944bfc4b4a08c14c2cc6859054cab
ChaeYubin <cyoobin@naver.com>
2025-02-02 13:46:27 +0900

    DEV-4 init: 프로젝트 세팅

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,7 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
+import TanstackQueryProviders from "@/lib/tanstack-query-provider";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import type { Metadata } from "next";
+import "@/styles/globals.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,11 +14,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="ko">
+      <body>
+        <TanstackQueryProviders>{children}</TanstackQueryProviders>
       </body>
     </html>
   );

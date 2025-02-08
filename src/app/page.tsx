1ae1a73146c80268e96a22788a318df9a9f5fe89
xorms571 <xorms6865@naver.com>
2025-02-08 09:05:21 +0900

    feat: jest, sentry, testing library setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,34 @@
-export default function Home() {
-  return <div />;
+"use client";
+
+import { useEffect } from "react";
+
+import * as Sentry from "@sentry/nextjs";
+import NextError from "next/error";
+
+import SentryCheck from "@/components/SentryCheck";
+
+export default function Home({
+  error,
+}: {
+  error?: Error & { digest?: string };
+}) {
+  useEffect(() => {
+    if (error) {
+      Sentry.captureException(error);
+    }
+  }, [error]);
+
+  if (!error) {
+    return (
+      <div>
+        <SentryCheck />
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <NextError statusCode={error.digest ? 500 : 404} />
+    </div>
+  );
 }

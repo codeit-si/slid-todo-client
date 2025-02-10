31aadbd68be9b675ca80fa1127004a8829013cc6
xorms571 <xorms6865@naver.com>
2025-02-10 19:01:39 +0900

    [DEV-72] refactor: 환경변수 다시 설정

--- src/app/api/sentry-api/route.ts
+++ src/app/api/sentry-api/route.ts
@@ -1,15 +1,15 @@
 import * as Sentry from "@sentry/nextjs";
 import { NextResponse } from "next/server";
 
-export const dynamic = "force-dynamic";
-
-export function GET() {
+export async function GET() {
   try {
+    // 의도적으로 에러를 발생시키는 코드
     throw new Error("Sentry Example API Route Error");
-  } catch (error: unknown) {
+  } catch (error) {
+    // Sentry에 에러 전송
     Sentry.captureException(error);
     return NextResponse.json(
-      { error: (error as Error).message },
+      { error: "Internal Server Error" },
       { status: 500 },
     );
   }

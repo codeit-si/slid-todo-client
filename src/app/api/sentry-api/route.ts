1bef9d2d12c07e81a46dd59ee95d348e987c4854
xorms571 <xorms6865@naver.com>
2025-02-10 16:55:39 +0900

    [DEV-72] refactor: sentry-check.tsx 수정

--- src/app/api/sentry-api/route.ts
+++ src/app/api/sentry-api/route.ts
@@ -1,3 +1,4 @@
+import * as Sentry from "@sentry/nextjs";
 import { NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
@@ -6,6 +7,7 @@ export function GET() {
   try {
     throw new Error("Sentry Example API Route Error");
   } catch (error: unknown) {
+    Sentry.captureException(error);
     return NextResponse.json(
       { error: (error as Error).message },
       { status: 500 },

a97dc6cc959ff7db8d5af0e32f586a64f9c99138
ChaeYubin <cyoobin@naver.com>
2025-02-12 18:34:09 +0900

    [DEV-35] refactor: 접근성 개선 및 불필요한 div 태그 제거

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -4,30 +4,33 @@ import { cva, VariantProps } from "class-variance-authority";
 
 import { cn } from "@/lib/cn";
 
-const ButtonVariants = cva("font-semibold px-18 py-8", {
-  variants: {
-    variant: {
-      solid:
-        "bg-purple-500 hover:bg-purple-600 active:bg-purple-800 disabled:bg-slate-400 text-white",
-      outlined:
-        "border border-purple-500 text-purple-500 hover:border-purple-600 hover:text-purple-600 active:border-purple-800 active:text-purple-800 disabled:border-slate-400 disabled:text-slate-400",
+const ButtonVariants = cva(
+  "flex items-center justify-center px-18 py-8 font-semibold",
+  {
+    variants: {
+      variant: {
+        solid:
+          "bg-purple-500 hover:bg-purple-600 active:bg-purple-800 disabled:bg-slate-400 text-white",
+        outlined:
+          "border border-purple-500 text-purple-500 hover:border-purple-600 hover:text-purple-600 active:border-purple-800 active:text-purple-800 disabled:border-slate-400 disabled:text-slate-400",
+      },
+      shape: {
+        square: "rounded-xl",
+        round: "rounded-3xl",
+      },
+      size: {
+        lg: "min-w-291 min-h-48 text-base",
+        md: "min-w-150 min-h-44 text-base",
+        sm: "min-w-84 min-h-36 text-sm",
+      },
     },
-    shape: {
-      square: "rounded-xl",
-      round: "rounded-3xl",
-    },
-    size: {
-      lg: "min-w-291 min-h-48 text-base",
-      md: "min-w-150 min-h-44 text-base",
-      sm: "min-w-84 min-h-36 text-sm",
+    defaultVariants: {
+      variant: "solid",
+      shape: "square",
+      size: "md",
     },
   },
-  defaultVariants: {
-    variant: "solid",
-    shape: "square",
-    size: "md",
-  },
-});
+);
 
 interface ButtonProps
   extends ButtonHTMLAttributes<HTMLButtonElement>,
@@ -42,15 +45,17 @@ export default function Button({
   shape,
   className,
   children,
+  disabled,
   ...props
 }: ButtonProps) {
   return (
     <button
       type={type}
       className={cn(ButtonVariants({ variant, shape, size }), className)}
+      aria-disabled={disabled}
       {...props}
     >
-      <div className="flex items-center justify-center">{children}</div>
+      {children}
     </button>
   );
 }
